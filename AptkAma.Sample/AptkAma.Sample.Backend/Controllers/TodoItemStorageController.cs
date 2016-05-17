79bf3f3a30b0e7198d2e61837752c2ec2fc00f85
Jérémy BRUN-PICARD <jeremy.brunpicard@apptracktive.com>
2016-05-17 14:35:52 +0200

    Adding File management to Sample.Backend

---
+++ AptkAma.Sample/AptkAma.Sample.Backend/Controllers/TodoItemStorageController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using AptkAma.Sample.Backend.DataObjects;
+using Microsoft.Azure.Mobile.Server.Config;
+using Microsoft.Azure.Mobile.Server.Files;
+using Microsoft.Azure.Mobile.Server.Files.Controllers;
+
+namespace AptkAma.Sample.Backend.Controllers
+{
+    [MobileAppController]
+    public class TodoItemStorageController : StorageController<TodoItem>
+    {
+        public TodoItemStorageController()
+        {
+
+        }
+
+        [HttpPost]
+        [Route("tables/TodoItem/{id}/StorageToken")]
+        public async Task<HttpResponseMessage> PostStorageTokenRequest(string id, StorageTokenRequest value)
+        {
+            StorageToken token = await GetStorageTokenAsync(id, value);
+
+            return Request.CreateResponse(token);
+        }
+
+        // Get the files associated with this record
+        [HttpGet]
+        [Route("tables/TodoItem/{id}/MobileServiceFiles")]
+        public async Task<HttpResponseMessage> GetFiles(string id)
+        {
+            IEnumerable<MobileServiceFile> files = await GetRecordFilesAsync(id);
+
+            return Request.CreateResponse(files);
+        }
+
+        [HttpDelete]
+        [Route("tables/TodoItem/{id}/MobileServiceFiles/{name}")]
+        public Task Delete(string id, string name)
+        {
+            return base.DeleteFileAsync(id, name);
+        }
+    }
+}
